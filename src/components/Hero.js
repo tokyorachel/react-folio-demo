c72238b83cb60abc913d66c0fc6b6f6b868992ad
rbatty <rbattybits@gmail.com>
2019-12-22 21:57:53 +0900

    Adds Hero component to replace Greeting placeholder.

---
+++ src/components/Hero.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import '../styles/hero.scss'
+
+const Hero = (props) => {
+
+  const [currentImage, setImage] = useState(
+    {
+      index: 0, 
+      path: props.slides[0]
+    });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setImage(prev => {
+        const newIndex = prev.index < props.slides.length - 1 ? prev.index + 1 : 0;
+        return {
+          index: newIndex,
+          path: props.slides[newIndex]
+        }
+      })
+      console.log(currentImage);
+    }, 7500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [])
+
+
+  return (
+    <section className="hero theme-dark">
+      { props.slides && 
+        <div className="hero-image" style={{ backgroundImage: "url("+currentImage.path+")" }}></div>
+      }
+      <div className="hero-overlay">
+        <h1 className="hero-title">Portfolio made easy.</h1>
+        <h2 className="hero-subtitle">Every creator needs a great portfolio.</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;
